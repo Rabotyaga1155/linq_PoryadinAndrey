23114358c96a6f188eb5e05cc5a6c13c80305f50
Rabotyaga1155 <123551706+Rabotyaga1155@users.noreply.github.com>
2023-03-01 14:10:44 +0500

    Add files via upload

---
+++ LINQ/LINQ/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace LINQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Люди младше 40";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(290, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Люди старше 40";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(502, 318);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 47);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(112, 98);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 76);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "zad2a";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(585, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(320, 98);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(156, 72);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "zad2b";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LINQ/LINQ/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LINQ
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public struct a
+        {
+            public string f;
+            public string i;
+            public string o;
+            public float age;
+            public float ves;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a str;
+            StreamReader FileInt = new StreamReader("text.txt");
+            string line;
+            Hashtable ludi = new Hashtable();
+            string a;
+            while ((line = FileInt.ReadLine()) != null)
+            {
+                string[] temp = line.Split(' ');
+                str.f = temp[0];
+                str.i = temp[1];
+                str.o = temp[2];
+                str.age = float.Parse(temp[3]);
+                str.ves = float.Parse(temp[4]);
+                if (str.age < 40)
+                {
+                    MessageBox.Show($"{str.f}  \t  {str.i}  \t  {str.o}  \t  {str.age}  \t  {str.ves}");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a str;
+            StreamReader FileInt = new StreamReader("text.txt");
+            string line;
+            Hashtable ludi = new Hashtable();
+            string a;
+            while ((line = FileInt.ReadLine()) != null)
+            {
+                string[] temp = line.Split(' ');
+                str.f = temp[0];
+                str.i = temp[1];
+                str.o = temp[2];
+                str.age = float.Parse(temp[3]);
+                str.ves = float.Parse(temp[4]);
+                if (str.age > 40)
+                {
+                    //label1.Text = ($"{str.f}  \t  {str.i}  \t  {str.o}  \t  {str.age}  \t  {str.ves}");
+                    MessageBox.Show($"{str.f}  \t  {str.i}  \t  {str.o}  \t  {str.age}  \t  {str.ves}");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            List<Employ> employes = new List<Employ>()
+            {
+            new Employ {Name="Иванов", Departament="Отдел закупок"},
+            new Employ {Name="Петров", Departament="Отдел закупок"},
+            new Employ {Name="Сидоров", Departament="Отдел продаж"},
+            new Employ {Name="Лямин", Departament="Отдел продаж"},
+            new Employ {Name="Сидоренко", Departament="Отдел маркетинга"},
+            new Employ {Name="Кривоносов", Departament="Отдел продаж"}
+            };
+
+            List<Departament> department = new List<Departament>()
+            {
+            new Departament {Name = "Отдел закупок", Reg="Германия"},
+            new Departament {Name = "Отдел продаж", Reg="Испания"},
+            new Departament {Name = "Отдел маркетинга", Reg="Испания"}
+            };
+
+            var result = from a in employes
+                         join t in department on a.Departament equals t.Name
+                         select new { Name = a.Name, Department = a.Departament, Reg = t.Reg };
+            foreach (var mb in result)
+            {
+                
+                label1.Text = ($"{mb.Name}-{mb.Department} ({mb.Reg}) \n" + label1.Text);
+            }
+
+
+
+
+
+
+
+
+
+
+
+            //List<Departament> department = new List<Departament>()
+            //{
+            //    new Departament { Name = "Отдел закупок", Reg ="Германия" },
+            //    new Departament { Name = "Отдел продаж", Reg ="Испания" },
+            //    new Departament { Name = "Отдел маркетинга", Reg ="Россия" }
+            //};
+            //List<Employ> employes = new List<Employ>()
+            //{
+            //        new Employ {Name="Иванов", Departament ="Отдел закупок"},
+            //        new Employ {Name="Петров", Departament ="Отдел закупок"},
+            //        new Employ {Name="Сидоров", Departament ="Отдел продаж"},
+            //        new Employ {Name="Лямин", Departament ="Отдел продаж "},
+            //        new Employ {Name="Сидоренко", Departament ="Отдел маркетинга"},
+            //        new Employ {Name="Кривоносов", Departament ="Отдел продаж "}
+            //};
+            //var result = from a in employes
+            //             join t in department on a.Departament equals t.Name
+            //             select new { Name = a.Name, Departament = a.Departament, Reg = t.Reg };
+            //foreach (var mb in result)
+            //{
+            //    //MessageBox.Show($"{mb.Name} - {mb.Departament} ({mb.Reg}) \n" + label1.Text);
+            //    label1.Text = ($"{mb.Name} - {mb.Departament} ({mb.Reg}) \n" + label1.Text);
+
+            //}
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+            //MessageBox.Show($"{item.Name} - {item.Dep} ({item.Reg})");
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<Employ> employes = new List<Employ>()
+            {
+            new Employ {Name="Иванов", Departament="Отдел закупок"},
+            new Employ {Name="Петров", Departament="Отдел закупок"},
+            new Employ {Name="Сидоров", Departament="Отдел продаж"},
+            new Employ {Name="Лямин", Departament="Отдел продаж"},
+            new Employ {Name="Сидоренко", Departament="Отдел маркетинга"},
+            new Employ {Name="Кривоносов", Departament="Отдел продаж"}
+            };
+
+            List<Departament> department = new List<Departament>()
+            {
+            new Departament {Name = "Отдел закупок", Reg="Германия"},
+            new Departament {Name = "Отдел продаж", Reg="Испания"},
+            new Departament {Name = "Отдел маркетинга", Reg="Испания"}
+            };
+
+            var result = from a in employes
+                         join t in department on a.Departament equals t.Name
+                         select new { Name = a.Name, Department = a.Departament, Reg = t.Reg };
+            foreach (var mb in result)
+            {
+                if (mb.Reg.StartsWith("И"))
+                    label2.Text = ($"{mb.Name}-{mb.Department} ({mb.Reg}) \n" + label2.Text);
+            }
+            
+        }
+    }
+}
